11866cb1fc7f9fe1ba9e28772eee953429544a48
fajar-dev <fajarrivaldi2015@gmail.com>
2025-07-23 10:46:13 +0700

    init: adding logo

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -32,6 +32,8 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Red,
             ])
+            ->brandLogo(asset('images/logo.png'))  
+            ->brandLogoHeight('50px') 
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
