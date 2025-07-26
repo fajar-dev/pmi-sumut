6d33a324cf4fa9b63d05905d9443ece75401bdcf
fajar-dev <fajarrivaldi2015@gmail.com>
2025-07-26 15:05:16 +0700

    fix: favicon

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -33,6 +33,7 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Red,
             ])
             ->brandLogo(asset('images/logo.png'))  
+            ->favicon(asset('images/favicon.png'))
             ->brandLogoHeight('50px') 
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
